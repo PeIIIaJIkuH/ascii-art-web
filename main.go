b646379fcaec8de97eb3210e1862497a2c2dd403
Yerkhan Baigal <baigal.erhan@gmail.com>
2020-05-27 02:37:27 +0600

    tests 8

--- main.go
+++ main.go
@@ -65,7 +65,7 @@ func indexHandler(w http.ResponseWriter, r *http.Request) {
 	output := string(out)
 
 	if len(output) == 0 {
-		temp := exec.Command("go test ./test hello")
+		temp := exec.Command("echo", "Hello World!")
 		temp.Stdin = os.Stdin
 		out1, _ := temp.Output()
 		output1 := string(out1)
